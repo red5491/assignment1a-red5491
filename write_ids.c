487bf2c6770335eb52fbd471f972c182344a0a75
Richard DeMenno <red5491@psu.edu>
2024-09-12 21:14:10 -0400

    first commit

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "red5491";
+    char STUDENT_ID[] = "949455191";
 
     return 0 ;
 }
